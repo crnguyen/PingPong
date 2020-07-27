1ab285f3a1edb763db2aad64603776ec69f03df5
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-07-26 22:19:10 -0700

    [html/css] change layout to canvas

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("my js is linked");
+
+function game() {
+    var canvas = document.getElementById("canvas");
+    this.width = canvas.width;
+    this.height = canvas.height;
+    this.context = canvas.getContext("2d");
+    this.context.fillStyle = "white";
+}
