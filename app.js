eb18c225619ffd6cd3b718da56619c0d8dde6baa
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-07-27 14:09:14 -0700

    create function that moves ball

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log("my js is linked");
 document.addEventListener("DOMContentLoaded", () => {
     // let paddle1;
     // let paddle2;
-    // let ball;
+    let ball = {};
     
     let canvas = document.getElementById("canvas");
     let ctx = canvas.getContext("2d");
@@ -35,24 +35,43 @@ let paddle2 = new Player(400, 426, 115, 33, "blue");
 // paddle2.render();
 
 const gameLoop = () => {
-    console.log("loopin ");
     //clear the canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     //display x,y coordinates of our hero onto the DOM
     paddle1.render(); 
     paddle2.render();
+    ball.draw();
+    moveTheBall();
 }
 
 //paddles appear on screen
 
 //create ball *
-// function ball() {
-//     ctx.beginPath();
-//     ctx.arc(90, 90, 25, 0, 2 * Math.PI, true);
-//     ctx.fillStyle = "black";
-//     ctx.fill();
-// }
-// ball();
+
+
+ball = {
+	x: 100,
+	y: 100, 
+	r: 10,
+	c: "black",
+	vx: 4,
+	vy: 8,
+	
+	// Function for drawing ball on canvas
+	draw: function() {
+		ctx.beginPath();
+		ctx.fillStyle = this.c;
+		ctx.arc(this.x, this.y, this.r, 0, Math.PI*2, false);
+		ctx.fill();
+	}
+};
+
+function moveTheBall() {
+    ball.x += ball.vx;
+	ball.y += ball.vy;
+}
+
+
 //create function that allows paddles to be moved at the same time in the same direction
 
 //create function that registers mouseover to move the paddles
@@ -70,8 +89,11 @@ const movePaddles = e => {
    } else if (e.keyCode === 68 && canvas.width > 0) {
        paddle2.x += 5;
    }
+   //requestAnimationFrame(movePaddles);
+
 }
 document.addEventListener("keydown", movePaddles);
+//requestAnimationFrame(movePaddles);
 //create function that detects hit (paddle to ball) and reverses the ball in the opposite direction at an angle
 
 //create an explosion sound when paddle hits ball
@@ -83,7 +105,7 @@ document.addEventListener("keydown", movePaddles);
 
 //create function that increases speed of ball after certain points/score
 
-let gamePlay = setInterval(gameLoop, 1);
+let gamePlay = setInterval(gameLoop, 50);
 
 })
 
