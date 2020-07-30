12d20eef3de8eea9f6fa4184b07516bd0c3977a7
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-07-29 22:00:53 -0700

    [JS] remove function that changes speed of ball- messes up my other functions

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-//console.log("my js is linked")
-document.addEventListener("DOMContentLoaded", () => {
-
-//when this is pressed, start game
-let startGame = document.getElementById("startGame");
-document.addEventListener("click", startGame);
-
-let gameInstructions = document.querySelector(".modal-btn");
-let modalBg = document.querySelector(".modal-bg");
-gameInstructions.addEventListener("click", function () {
-    modalBg.classList.add("bg-active");
-});
-
-let modalClose = document.querySelector(".modal-close");
-modalClose.addEventListener("click", function () {
-    modalBg.classList.remove("bg-active");
-})
-
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let score = 1;
 let gameScore = document.getElementById("score");
+let startGame = document.getElementById("startGame");
+let gameInstructions = document.querySelector(".modal-btn");
+let modalBg = document.querySelector(".modal-bg");
+let modalClose = document.querySelector(".modal-close");
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+startGame.addEventListener("click", moveTheBall);
+
+//modal
+gameInstructions.addEventListener("click", function () {
+    modalBg.classList.add("bg-active");
+});
+
+modalClose.addEventListener("click", function () {
+    modalBg.classList.remove("bg-active");
+})
 
 //create function to draw paddle *
 function Player(x, y, width, height, color) {
@@ -37,16 +37,6 @@ function Player(x, y, width, height, color) {
 let paddle1 = new Player(400, 1, 115, 33, "green");
 let paddle2 = new Player(400, 426, 115, 33, "blue");
 
-const gameLoop = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    paddle1.render(); 
-    paddle2.render();
-    ball.draw();
-    moveTheBall();
-    detectHit();
-    //changeSpeedOfBall();
-}
-
 //create ball *
 ball = {
 	x: 400,
@@ -60,8 +50,8 @@ ball = {
 	draw: function() {
 		ctx.beginPath();
 		ctx.fillStyle = this.c;
-		ctx.arc(this.x, this.y, this.r, 0, Math.PI*2, false);
-		ctx.fill();
+		ctx.arc(this.x, this.y, this.r, 0, Math.PI*2);
+        ctx.fill();
 	}
 };
 
@@ -69,30 +59,18 @@ function moveTheBall() {
     //this is what gets the ball moving
     ball.x += ball.vx;
     ball.y += ball.vy;
-    
-    if(ball.x + ball.vx > canvas.width - ball.r || ball.x + ball.vx < ball.r) {
-        console.log("bounce off wall"); 
-        ball.vx = -ball.vx;
-    } else if(ball.y + ball.vy > canvas.height - ball.r || ball.y + ball.vy < ball.r) {
-        gameOver();
-        gameScore.innerHTML = "GAME OVER! YOU LOSE!";
-        clearInterval(gamePlay);
-        //console.log("you lose"); // this is where you lose and game stops
-        //ball stops moving!!
-        // ball.x -= ball.vx;
-        // ball.y -= ball.vy;
-    }
 }
 
 function increaseScore() {
     gameScore.innerHTML = "Score: " +score++;
-    if (score === 21) {
+    if (score === 11) {
         //console.log("you win");
         setInterval(function() {gameScore.innerHTML = "GAME OVER! YOU WIN!"},30);
         gameOver();
-    } else if (score === 10) {
-        changeSpeedOfBall();
-    }
+        clearInterval(gamePlay);
+    // } else if (score === 5) {
+    //    changeSpeedOfBall();
+     }
 }
 
 function gameOver() {
@@ -100,19 +78,25 @@ function gameOver() {
 }
 
 function detectHit () {
+    if(ball.x + ball.vx > canvas.width - ball.r || ball.x + ball.vx < 0) {
+        console.log("bounce off wall"); 
+        ball.vx = -ball.vx;
+    } else if(ball.y + ball.vy > canvas.height - ball.r || ball.y + ball.vy < 0) {
+        gameOver();
+        gameScore.innerHTML = "GAME OVER! YOU LOSE!";
+        clearInterval(gamePlay);
+        //console.log("you lose"); // this is where you lose and game stops
+    }
+
     if (ball.x + ball.vx > paddle1.x &&
         ball.x < paddle1.x + paddle1.width 
         && ball.y + ball.r > paddle1.y &&
         ball.y < paddle1.y + paddle1.height){
-            if (ball.y < 28) {
+            if (ball.y > 28) {
                 ball.y = 28;
-                //console.log("HELLO");
             }
             increaseScore();
             ball.vy = -ball.vy;
-            //console.log("ball.vy", ball.vy);
-            //console.log("hit");
-            //console.log("----------");
 } 
     else if (ball.x + ball.vx > paddle2.x &&
     ball.x < paddle2.x + paddle2.width &&
@@ -120,21 +104,10 @@ function detectHit () {
     ball.y < paddle2.y + paddle2.height) {
          if (ball.y > 420) {
             ball.y = 420;
-            //console.log("second hit ------");
         }
         increaseScore();
-        ball.vy = -ball.vy;
-        
+        ball.vy = -ball.vy;      
 }}
-
-function changeSpeedOfBall() {
-    if(ball.vx) {
-        ball.vx = 8;
-    }
-    if (ball.vy) {
-        ball.vy = 15;
-    }
-    };
 
 //create function that moves paddles using keys
 const movePaddles = e => {
@@ -153,6 +126,16 @@ const movePaddles = e => {
    };
 }
 document.addEventListener("keydown", movePaddles);
+
+
+const gameLoop = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    paddle1.render(); 
+    paddle2.render();
+    ball.draw(); 
+    moveTheBall();
+    detectHit();
+}
 
 let gamePlay = setInterval(gameLoop, 50);
 })
